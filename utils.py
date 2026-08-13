789cd73b5c61184226a27b6bdfd8440397622a59
aditya-jyrj <aditya.jyrj@gmail.com>
2026-08-14 01:06:45 +0800

    standardised side length to L instead of N, implemented periodic boundary conditions and made saving lattice snapshots optional

--- utils.py
+++ utils.py
@@ -3,82 +3,84 @@ import matplotlib.pyplot as plt
 import numba
 from numba import njit
 from scipy.ndimage import convolve, generate_binary_structure
-
 import imageio.v2 as imageio
-from IPython.display import Image
+
+def generate_lattice(L, ratio=0.5):
+    init_random = np.random.random((L,L))
+    lattice = np.zeros((L, L))
+    lattice[init_random<=ratio] = 1
+    lattice[init_random>ratio] = -1
+
+    return lattice
 
 def get_energy(lattice, H=0.0):
     # nearest neighbours summation
     kern = generate_binary_structure(2, 1) 
     kern[1][1] = False
 
-    interaction = -lattice * convolve(lattice, kern, mode='constant', cval=0) 
+    interaction = -lattice * convolve(lattice, kern, mode='wrap') 
     field = -H * lattice
     
     return interaction.sum() / 2 + field.sum()
 
+
 # numba speeds up the iterations to almost C speed
 @numba.njit 
-def metropolis(lattice, steps, BJ, H, energy, save_every):
-    N = lattice.shape[0]
-    if N != lattice.shape[1]:
+def metropolis(lattice, steps, BJ, H, energy, save_every=None):
+    L = lattice.shape[0]
+    if L != lattice.shape[1]:
         raise ValueError("metropolis is configured for only square lattices")
 
-    # 1. call current state mu
-    lattice = lattice.copy()
+    if save_every is not None:
+        n_frames = steps // save_every + 1
+        frames = np.empty((n_frames, L, L), dtype=np.int8)
+        frames[0] = lattice
+        frame_idx = 1
+    else:
+        frames = None
+
     net_spins = np.zeros(steps-1)
     net_energies = np.zeros(steps-1)
 
-    n_frames = steps // save_every + 1
-    frames = np.empty((n_frames, N, N), dtype=np.int8)
-
-    frames[0] = lattice
-    frame_idx = 1
+    # 1. call current state mu
+    lattice = lattice.copy()
 
     for t in range(0, steps-1):
 
         # 2. pick random spins and flip it
-        x = np.random.randint(0,N)
-        y = np.random.randint(0,N)
+        x = np.random.randint(0,L)
+        y = np.random.randint(0,L)
 
-        spin_i = lattice[x,y]
-        spin_f = spin_i * -1 # proposed sign flip
+        spin = lattice[x,y]
 
         # compute change in energy
-        E_i = -H * spin_i
-        E_f = -H * spin_f
-        if x>0:
-            E_i += -spin_i*lattice[x-1,y]
-            E_f += -spin_f*lattice[x-1,y]
-        if x<N-1:
-            E_i += -spin_i*lattice[x+1,y]
-            E_f += -spin_f*lattice[x+1,y]
-        if y>0:
-            E_i += -spin_i*lattice[x,y-1]
-            E_f += -spin_f*lattice[x,y-1]
-        if y<N-1:
-            E_i += -spin_i*lattice[x,y+1]
-            E_f += -spin_f*lattice[x,y+1]
+        neighbour_sum = (
+              lattice[(x - 1) % L, y]
+            + lattice[(x + 1) % L, y]
+            + lattice[x, (y - 1) % L]
+            + lattice[x, (y + 1) % L]
+        )
+        dE = 2 * spin * (neighbour_sum + H)
 
         # 3 / 4. change state with designated probabilities
-        dE = E_f-E_i
         if dE <= 0:
-            lattice[x, y] = spin_f
+            lattice[x, y] *= -1
             energy += dE
         elif np.random.random() < np.exp(-BJ * dE):
-            lattice[x, y] = spin_f
+            lattice[x, y] *= -1
             energy += dE
             
         net_spins[t] = lattice.sum()
         net_energies[t] = energy
 
-        if (t+1) % save_every == 0:
+        if save_every is not None and (t+1) % save_every == 0:
             frames[frame_idx] = lattice
             frame_idx += 1
             
     return lattice, net_spins, net_energies, frames
 
-def make_gif(frames, filename="ising_evolution.gif", fps=10):
+
+def make_gif(frames, filename="ising_evolution.gif", fps=16):
     images = []
 
     for frame in frames:
